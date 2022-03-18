079f92cf68ad70c40b328a03aae770e1d4e1bb2b
hassan141 <hassansamy491@gmail.com>
2022-03-18 15:22:05 +0200

    plot

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from PyQt5 import QtWidgets, uic
-import sys 
-from numpy import sin, pi, arange  # ceil, floor, linspace,
-import matplotlib.pyplot as plt
-
+from pickle import GLOBAL
+from PyQt5 import QtWidgets, QtCore, uic
+from PyQt5.QtWidgets import QSlider
+import pyqtgraph as pg
+import sys  # We need sys so that we can pass argv to QApplication
+import os
+import pathlib
+import numpy as np
+import pandas as pd
+from numpy import sin, pi, arange
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -13,12 +18,34 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # calling the compose function using the compose button
         self.compose_button.clicked.connect(self.compose)
+        self.action_open.triggered.connect(self.open)
+        self.draw_button.clicked.connect(self.draw)
 
         # initializing frequency, magnitude, and phase shift variables
         self.frequency = 1
         self.magnitude = 1
         self.phase_shift = 0
-
+    def open(self):
+        self.main_signal_widget.clear()
+        self.main_signal_widget.setBackground('w')
+        files_name = QtWidgets.QFileDialog.getOpenFileName(self, 'Open only CSV ', os.getenv('HOME'), "csv(*.csv)")
+        path = files_name[0]
+        
+        pathlib.Path(path).suffix == ".csv"
+        data = pd.read_csv(path)
+        self.time_col = data.values[:, 0]
+        self.amp_col = data.values[:, 1]
+        FtAmp = np.fft.fft(self.amp_col)
+        FtAmp = FtAmp[0:int(len(self.amp_col) /2)]
+        FtAmp = abs(FtAmp)
+        maxpower = max(FtAmp)
+        noise = (maxpower / 10)
+        self.fmaxtuble = np.where(FtAmp > noise)
+        self.maxFreq = max(self.fmaxtuble[0])
+        print(self.maxFreq)
+        
+    
+    
     def compose(self):
         # clearing the widget so that there aren't several plots on top of each other
         self.composer_widget.clear()
@@ -31,6 +58,7 @@ class MainWindow(QtWidgets.QMainWindow):
         # sinusoidal changing with input frequency, magnitude, and phase shift
         signal = self.magnitude * sin(2 * pi * self.frequency * time + self.phase_shift) 
         self.composer_widget.plot(time, signal)  # plotting
+        
        
   
         
