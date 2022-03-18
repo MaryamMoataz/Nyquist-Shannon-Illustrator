9edce58f7587af14ead0faeca6ad49557351be68
Clara Ashraf <97061363+claraashraf24@users.noreply.github.com>
2022-03-19 00:51:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,19 @@ import pathlib
 import numpy as np
 import pandas as pd
 from numpy import sin, pi, arange
+from mainwindow import Ui_MainWindow
 
 import matplotlib.pyplot as plt
-
+counter=-1
 
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         #Load the UI Page
         super(MainWindow, self).__init__(*args, **kwargs)
-        uic.loadUi('mainwindow.ui', self)
+        # uic.loadUi('mainwindow.ui', self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
          # initializing frequency, magnitude, and phase shift variables
         self.frequency = 1
         self.magnitude = 1
@@ -28,9 +31,16 @@ class MainWindow(QtWidgets.QMainWindow):
         self.time = arange(0.0, 5.0, 0.02) # start:0, end:5, step:200 ms intervals
         
         # calling the compose function using the compose button
-        self.compose_button.clicked.connect(self.compose)
+        self.ui.compose_button.clicked.connect(self.compose)
 
-        self.action_open.triggered.connect(self.open)
+        self.ui.action_open.triggered.connect(self.open)
+        self.ui.confirm_button.clicked.connect(self.composer_summation)
+        self.ui.delete_button.clicked.connect(self.delete_signal)
+        self.ui.horizontal_slider.valueChanged.connect(self.sampling)
+        self.ui.draw_button.clicked.connect(self.plotSeparateSamples)
+        self.ui.action_save.triggered.connect(self.save)
+        self.ui.move_to_main.clicked.connect(self.move_to_main)
+        # self.combobox.currentIndexChanged.connect(self.change_value)
         
 
         # initializing frequency, magnitude, and phase shift variables
@@ -39,7 +49,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.phase_shift = 0
 
     def open(self):
-        self.main_signal_widget.clear()
+        self.ui.main_signal_widget.clear()
         #self.main_signal_widget.setBackground('w')
         files_name = QtWidgets.QFileDialog.getOpenFileName(self, 'Open only CSV ', os.getenv('HOME'), "csv(*.csv)")
         path = files_name[0]
@@ -56,42 +66,107 @@ class MainWindow(QtWidgets.QMainWindow):
         self.fmaxtuble = np.where(FtAmp > noise)
         self.maxFreq = max(self.fmaxtuble[0])
         print(self.maxFreq)
-        self.main_signal_widget.plot(self.time_col, self.amp_col,pen='blue')
+        self.ui.main_signal_widget.plot(self.time_col, self.amp_col,pen='blue')
         #freqs = np.fft.fftfreq(len(self.amp_col))
         #print(freqs)
     
-        self.main_signal_widget.plot(self.time_col, self.amp_col)
+        self.ui.main_signal_widget.plot(self.time_col, self.amp_col)
         #freqs = np.fft.fftfreq(len(self.amp_col))
-        #print(freqs)   
+        #print(freqs)
+
+    def changedvalue(self):
+        self.min_slider = 0
+        self.max_slider = 3 * self.maxFreq
+        self.ui.horizontal_slider.setMinimum(self.min_slider)
+        self.ui.horizontal_slider.setMaximum(int(self.max_slider))
+        self.sampling_factor = self.ui.horizontal_slider.value()
+        print(self.sampling_factor)
+        self.ui.horizontal_slider.setTickInterval(int(1 + self.maxFreq))
+        return self.sampling_factor
+
+    def sampling(self):
+        self.ui.main_signal_widget.clear()
+        self.changedvalue()
+
+        if self.sampling_factor == 0:
+
+            self.ui.main_signal_widget.plot(self.time_col, self.amp_col)
+
+        else:
+            self.size = int(len((self.time_col))) - 1
+            self.step = int(self.size / self.ui.horizontal_slider.value())
+            self.data_sampled_list = [self.amp_col[0]]
+            self.time_sampled_list = [self.time_col[0]]
+            for index in range(int(self.step), self.size + 1, int(self.step)):
+                self.data_sampled_list.append(self.amp_col[index])
+                self.time_sampled_list.append(self.time_col[index])
+            self.Data_sampled_nparray = np.array(self.data_sampled_list)
+            self.time_sampled_nparray = np.array(self.time_sampled_list)
+
+            self.ui.main_signal_widget.plot(self.time_col, self.amp_col, pen='red')
+            self.ui.main_signal_widget.plot(self.time_sampled_nparray, self.Data_sampled_nparray, symbol="o")
+
+    def plotSeparateSamples(self):
+        self.ui.reconstructed_graph_widget.clear()
+        self.ui.reconstructed_graph_widget.plot(self.time_sampled_nparray, self.Data_sampled_nparray, pen='blue')
+        self.ui.reconstructed_graph_widget.setBackground('black')
 
     def compose(self):
         # clearing the widget so that there aren't several plots on top of each other (overlapped)
-        self.composer_widget.clear()
+        self.ui.composer_widget.clear()
         
         # reading the inputs from the UI line edit elements
-        self.frequency = float(self.freq_line_edit.text()) # frequency
-        self.magnitude = float(self.magnitude_line_edit.text() ) # magnitude
-        self.phase_shift = float(self.phase_line_edit.text()) # phase shift
+        self.frequency = float(self.ui.freq_line_edit.text()) # frequency
+        self.magnitude = float(self.ui.magnitude_line_edit.text() ) # magnitude
+        self.phase_shift = float(self.ui.phase_line_edit.text()) # phase shift
         
         #time = arange(0.0, 5.0, 0.02) # start:0, end:5, step:200 ms intervals
         # sinusoidal changing with input frequency, magnitude, and phase shift
 
         signal = self.magnitude * sin(2 * pi * self.frequency * self.time + self.phase_shift) 
-        self.composer_widget.plot(self.time, signal)  # plotting
+        self.ui.composer_widget.plot(self.time, signal)  # plotting
         
         self.signal = self.magnitude * sin(2 * pi * self.frequency * self.time + self.phase_shift) 
         
-        self.composer_widget.plot(self.time, self.signal)  # plotting
-        
+        self.ui.composer_widget.plot(self.time, self.signal)  # plotting
+
+
+
           
 
     def composer_summation(self):
-        self.summation_graph_widget.clear()
+        global counter
+        self.ui.summation_graph_widget.clear()
         #time = arange(0.0, 5.0, 0.02) # start:0, end:5, step:200 ms intervals
         
         self.sum_of_signals.append(self.signal) # appends an element to the end of the list
-          
-        self.summation_graph_widget.plot(self.time, sum(self.sum_of_signals))  # plotting
+        self.ui.combobox.addItem(str(counter + 1))
+        self.ui.summation_graph_widget.plot(self.time, sum(self.sum_of_signals))  # plotting
+
+        counter=counter+1
+
+    def move_to_main(self):
+         self.ui.main_signal_widget.clear()
+         self.ui.main_signal_widget.plot(self.time, sum(self.sum_of_signals))
+
+    def delete_signal(self):
+        global counter
+        # if self.ui.combobox.currentIndex() == -1:
+        #     self.sum_of_signals.pop(self.sum_of_signals[0])
+
+
+        index = self.ui.combobox.currentIndex()-1
+        self.ui.combobox.removeItem(self.ui.combobox.currentIndex())
+        print(index)
+        self.sum_of_signals.pop(index)
+        print(index)
+
+        self.ui.summation_graph_widget.clear()
+        self.ui.summation_graph_widget.plot(self.time, sum(self.sum_of_signals))
+
+            # time = arange(0.0, 5.0, 0.02) # start:0, end:5, step:200 ms intervals
+            # self.summation_graph_widget.plot(self.time, sum(self.sum_of_signals))
+
 
 
     def save(self):
